68eb8d94548d061fafe28cb3e84191389731990c
Ryadh Khsib <ryadh.khsib@gmail.com>
2019-09-24 17:07:34 +0100

    added model file.

---
+++ src/SpectralNetModel.py
@@ -0,0 +1,105 @@
+import argparse
+
+import joblib
+
+from applications.config import get_spectralnet_config
+from core import networks
+from core.data import build_spectral_data
+from core.util import get_y_preds_from_cm
+import tensorflow as tf
+import keras.backend.tensorflow_backend as ktf
+import os
+from keras.layers import Input
+import numpy as np
+
+# Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=str, help='gpu number to use', default='')
+parser.add_argument('--gpu_memory_fraction', type=float, help='gpu percentage to use', default='1.0')
+parser.add_argument('--dset', type=str, help='dataset to use', default='mnist')
+args = parser.parse_args()
+
+
+def get_session(gpu_fraction=0.333):
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction,
+                                allow_growth=False)
+    return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+
+class SpectralNetModel(object):
+
+    def __init__(self):
+        # self.c = metric.MetricClient()
+        global graph
+        graph = tf.get_default_graph()
+
+        params = get_spectralnet_config(args)
+
+        # LOAD DATA
+        data = build_spectral_data(params)
+
+        ktf.set_session(get_session(args.gpu_memory_fraction))
+
+        x_train, y_train, x_val, y_val, x_test, y_test = data['spectral']['train_and_test']
+
+        x = x_train[:100]
+        np.savetxt('spectralnet_input.txt', x)
+        batch_sizes = {
+            'Unlabeled': x.shape[0],
+            'Labeled': x.shape[0],
+            'Orthonorm': x.shape[0],
+        }
+
+        input_shape = x.shape[1:]
+
+        y_labeled_onehot = np.empty((0, params['n_clusters']))
+
+        # spectralnet has three inputs -- they are defined here
+        inputs = {
+            'Unlabeled': Input(shape=input_shape, name='UnlabeledInput'),
+            'Labeled': Input(shape=input_shape, name='LabeledInput'),
+            'Orthonorm': Input(shape=input_shape, name='OrthonormInput'),
+        }
+
+        # Load Siamese network
+        if params['affinity'] == 'siamese':
+            siamese_net = networks.SiameseNet(inputs, params['arch'], params.get('siam_reg'), None,
+                                              params['siamese_model_path'])
+
+        else:
+            siamese_net = None
+
+        # Load Spectral net
+        y_true = tf.placeholder(tf.float32, shape=(None, params['n_clusters']), name='y_true')
+
+        spectralnet_model_path = os.path.join(params['model_path'], 'spectral_net')
+        self.spectral_net = networks.SpectralNet(inputs, params['arch'],
+                                                 params.get('spec_reg'),
+                                                 y_true, y_labeled_onehot,
+                                                 params['n_clusters'], params['affinity'], params['scale_nbr'],
+                                                 params['n_nbrs'], batch_sizes,
+                                                 spectralnet_model_path,
+                                                 siamese_net, train=False
+                                                 )
+        # get final embeddings
+        self.km = joblib.load(os.path.join(params['model_path'], 'spectral_net', 'kmeans.sav'))
+
+        self.confusion_matrix = joblib.load(os.path.join(params['model_path'], 'spectral_net', 'confusion_matrix.sav'))
+        self.params = params
+        self.x_train = x_train
+
+    def predict(self, X, feature_names):
+        with graph.as_default():
+            x = np.concatenate([self.x_train, X], axis=0)
+            x_spectralnet = self.spectral_net.predict_unlabelled(x)
+
+            kmeans_assignments = self.km.predict(x_spectralnet)
+            y_spectralnet = get_y_preds_from_cm(kmeans_assignments, self.params['n_clusters'], self.confusion_matrix)
+            return y_spectralnet[-X.shape[0]:]
+
+    def send_feedback(self, features, feature_names, reward, truth):
+        pass
+
+
+if __name__ == '__main__':
+    d = SpectralNetModel()
